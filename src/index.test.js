c0e8b4db924a8c87ab02409061a49b25498167b2
Tarin Pratt <tarinpratt@gmail.com>
2019-06-26 17:24:43 -0700

    added test suite and snapshots

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import List from './list';
 import Card from './card';
+import renderer from 'react-test-renderer';
 
+
+describe('Lists component', ()=> {
 it('renders without crashing', () =>{
     const div = document.createElement('div');
     ReactDOM.render(<Card />, div);
@@ -11,6 +14,21 @@ it('renders without crashing', () =>{
 
 it('renders without crashing', () =>{
     const section = document.createElement('section');
-    ReactDOM.render(<List />, section);
+    ReactDOM.render(<List cards={[]} />, section);
     ReactDOM.unmountComponentAtNode(section);
 });
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List cards={[]} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();  
+    });
+
+    it('renders the UI as expected', () => {
+        const tree = renderer
+          .create(<Card />)
+          .toJSON();
+        expect(tree).toMatchSnapshot();  
+        });
+    });
