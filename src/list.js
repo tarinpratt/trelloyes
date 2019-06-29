fb7930b955fc3349458021f3cb0c196c2631b703
Tarin Pratt <tarinpratt@gmail.com>
2019-06-29 15:27:58 -0700

    callback props

--- src/list.js
+++ src/list.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import Card from './card';
+import Card from './card'
 import './list.css';
 
-function List(props) {
-    return <section className="List">
-        <header>
-            {props.header}
+export default function List(props) {
+    return (
+      <section className='List'>
+        <header className='List-header'>
+          <h2>{props.header}</h2>
         </header>
-        <div className="List-cards">
+        <div className='List-cards'>
           {props.cards.map((card) =>
-            <Card 
-            key={card.id}
-            title={card.title}
-            content={card.content}
+            <Card
+              key={card.id}
+              id={card.id}
+              title={card.title}
+              content={card.content}
+              onClickDelete={props.onClickDelete}
             />
-            )}
-        </div>
-        <button type="button" className="List-add-button">
+          )}
+          <button
+            type='button'
+            className='List-add-button'
+            onClick={() => props.onClickAdd(props.id)}
+          >
             + Add Random Card
           </button>
-    </section>
-}
-
-export default List;
+        </div>
+      </section>
+    )
+  }
+  
+  List.defaultProps = {
+    onClickAdd: () => {},
+  }
